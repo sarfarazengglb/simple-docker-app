f08150731af536fb57d013efdd681ae88aacc06b
Sarfaraz-Save Tiger, Save Forest <sarfarazengglb@gmail.com>
2023-07-30 02:52:21 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// app.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!');
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
